cf4c5ab88f389b45b06f6d94397d2ce3c839a72f
Khalilelk20 <139553035+Khalilelk20@users.noreply.github.com>
2025-03-06 22:24:59 +0100

    Delete micro-services-project directory

--- micro-services-project/gce.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_disk" "mysql_disk" {
-  name  = "mysql-disk"
-  size  = 1   
-  type  = "pd-standard"  
-  zone  = "us-central1-a" 
-}
-
--- micro-services-project/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = "pfs-khalil-jihen"
-  region = "us-central1"
-  zone = "us-central1-a"
-}
